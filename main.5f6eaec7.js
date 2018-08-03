20fa3f90958c014d24b6ec03223e40775b0f4e94
Per Lundkvist <per.lundkvist@bonniernews.se>
2018-08-03 15:36:39 +0200

    Updates

--- main.5f6eaec7.js
+++ main.5f6eaec7.js
@@ -134,7 +134,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '49887' + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '52942' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
